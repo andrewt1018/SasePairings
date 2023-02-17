7c2b54668c8a5dfdb9381070b8e597d5fd734fa9
andrewtan1018 <tan434@purdue.edu>
2023-02-17 15:23:32 -0500

    Added and created all of the mentor groups

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Main {
     // ArrayList to store every single mentee
-    private static ArrayList<Person> mentees = new ArrayList<>();
+    private static ArrayList<Mentee> mentees = new ArrayList<>();
 
     // ArrayList to store every single mentee
     private static ArrayList<Person> mentors = new ArrayList<>();
+
+    // ArrayList to store all mentor groups
+    private static ArrayList<MentorGroup> mentorGroups = new ArrayList<>();
 
     // ArrayList to store all of the mentees whose information are not complete
     private static ArrayList<String> err_mentees = new ArrayList<>();
@@ -15,13 +20,66 @@ public class Main {
     private static ArrayList<String> err_mentors = new ArrayList<>();
 
     public static void main(String[] args) {
-        // Create the mentors
-        readMentors("mentors.txt");
         // Create mentees
         readMentees("in.txt", "out.txt");
         for (Person mentee : mentees) {
             System.out.println(mentee);
         }
+
+        // Create the mentors
+        readMentors("mentors.txt");
+
+        // Create the mentor groups
+        createMentorGroups();
+
+    }
+
+    public static void createMentorGroups() {
+        ArrayList<Person> group1, group2, group3, group4, group5, group6, group7;
+        group1 = (ArrayList<Person>) mentors.stream()
+                                            .filter(n -> (n.getName().equals("Andre Tan") ||
+                                                        (n.getName().equals("Kerway Tsai")) ||
+                                                        (n.getName().equals("Shivam Duhan"))))
+                                            .collect(Collectors.toCollection(ArrayList::new));
+        group2 = (ArrayList<Person>) mentors.stream()
+                                            .filter(n -> (n.getName().equals("Kyle Yang") ||
+                                                        (n.getName().equals("Claire Huang")) ||
+                                                        (n.getName().equals("Matthew Kumnoonsate"))))
+                                            .collect(Collectors.toCollection(ArrayList::new));
+        group3 = (ArrayList<Person>) mentors.stream()
+                                            .filter(n -> (n.getName().equals("Duclan Ngo") ||
+                                                        (n.getName().equals("Alia Rumreich")) ||
+                                                        (n.getName().equals("Jackie Fung He"))))
+                                            .collect(Collectors.toCollection(ArrayList::new));
+        group4 = (ArrayList<Person>) mentors.stream()
+                                            .filter(n -> (n.getName().equals("Alex Zhuo") ||
+                                                        (n.getName().equals("Janine Lee")) ||
+                                                        (n.getName().equals("Ethan Chu"))))
+                                            .collect(Collectors.toCollection(ArrayList::new));
+        group5 = (ArrayList<Person>) mentors.stream()
+                                            .filter(n -> (n.getName().equals("Dillon Kim") ||
+                                                        (n.getName().equals("Emily Yeh")) ||
+                                                        (n.getName().equals("Winston Wang"))))
+                                            .collect(Collectors.toCollection(ArrayList::new));
+        group6 = (ArrayList<Person>) mentors.stream()
+                                            .filter(n -> (n.getName().equals("Caroline Wang") ||
+                                                        (n.getName().equals("Abby Zahm")) ||
+                                                        (n.getName().equals("Janathan Balakrishnan"))))
+                                            .collect(Collectors.toCollection(ArrayList::new));
+        group7 = (ArrayList<Person>) mentors.stream()
+                                            .filter(n -> (n.getName().equals("Yong Sung Kim") ||
+                                                        (n.getName().equals("Jessica Wong")) ||
+                                                        (n.getName().equals("Michael Bi"))))
+                                            .collect(Collectors.toCollection(ArrayList::new));
+
+        // Add all of the mentor groups to the list
+        mentorGroups.add(new MentorGroup(group1));
+        mentorGroups.add(new MentorGroup(group2));
+        mentorGroups.add(new MentorGroup(group3));
+        mentorGroups.add(new MentorGroup(group4));
+        mentorGroups.add(new MentorGroup(group5));
+        mentorGroups.add(new MentorGroup(group6));
+        mentorGroups.add(new MentorGroup(group7));
     }
 
     public static void readMentors(String in_file) {
@@ -56,6 +114,7 @@ public class Main {
             e.printStackTrace();
         }
     }
+
     public static void readMentees(String in_file, String out_file) {
         // Retrieving all of the mentees' data from a file
         // Expected file layout: name,major,email,discordID,year,preferred_mentor1,preferred_mentor2,preferred_mentor3
@@ -81,11 +140,11 @@ public class Main {
                 String mentor3 = lineArr[7];
 
                 // Checks whether the current person already exists in the file using
-                // their email as the primary key
+                // their email and discord ID as the primary key
                 File fcheck = new File(out_file);
                 if (fcheck.exists()) {
                     String out_file_text = readFileAsString(out_file);
-                    if (out_file_text.contains(email)) {
+                    if (out_file_text.contains(email) && out_file_text.contains(discordID)) {
                         line = bfr.readLine();
                         continue;
                     }
@@ -105,7 +164,7 @@ public class Main {
         try {
             out.createNewFile();
             FileWriter fw = new FileWriter(out_file, true);
-            for (Person p : mentees) {
+            for (Mentee p : mentees) {
                 fw.append(p.toString());
             }
             fw.close();
--- src/MentorGroup.java
+++ src/MentorGroup.java
@@ -6,4 +6,13 @@ public class MentorGroup {
     public MentorGroup(ArrayList<Person> mentors) {
         this.mentors = mentors;
     }
+
+    @Override
+    public String toString() {
+        String ret = "";
+        for (int i = 0; i < mentors.size(); i++) {
+            ret += String.format("Mentor %d: %s\n", i, mentors.get(i).getName());
+        }
+        return ret;
+    }
 }
